b244462cd36f6c0938b13375a12dd2c2d61eb653
pavelsannikov <pavelsan95@mail.ru>
2022-09-04 18:18:42 +0500

    Добавьте файлы проекта.

---
+++ Module 5.6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+
+    }
+
+    static (string name_first, string name_last, int age, int pet_number, string[] pet_list, int color_number, string[] color_list) AddUser()
+    {
+        (string name_first, string name_last, int age, int pet_number, string[] pet_list, int color_number, string[] color_list) User;
+
+        Console.WriteLine("Введите своё имя: ");
+        User.name_first = Console.ReadLine();
+
+
+
+
+
+
+        /*
+
+
+        int number;
+
+        bool success = int.TryParse(value, out number);
+        if (success)
+        {
+            Console.WriteLine($"Converted '{value}' to {number}.");
+        }
+        else
+        {
+            Console.WriteLine($"Attempted conversion of '{value ?? "<null>"}' failed.");
+        }
+        */
+        return User;
+    }
+}
